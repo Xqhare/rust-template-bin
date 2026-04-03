8ad947cee024205d8545e9a6b86945206d72cb7d
Xqhare <xqhare@web.de>
2026-04-03 17:41:51 +0200

    update template, add automation startup script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::all)]
+#![warn(clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::print_stdout,
+    clippy::implicit_return
+)]
 
 fn main() {
     println!("Hello, world!");
